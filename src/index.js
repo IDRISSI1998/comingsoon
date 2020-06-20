b9f7753b568e0e4ec9bcac695a8cff9e02c2885b
Goodman Luphondo <luphondog@icloud.com>
2020-06-20 15:16:56 +0200

    Pricing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var launchDate = new Date("June 15, 2020 00:00:00").getTime();
+  // var launchDate = new Date("June 15, 2020 00:00:00").getTime();
 
-  var launch = setInterval(function () {
-    var now = new Date().getTime();
-    var dif = launchDate - now;
+  // var launch = setInterval(function () {
+  //   var now = new Date().getTime();
+  //   var dif = launchDate - now;
 
-    var days = Math.floor(dif / (1000 * 60 * 60 * 24));
-    var hrs = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var mins = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
-    var secs = Math.floor((dif % (1000 * 60)) / 1000);
+  //   var days = Math.floor(dif / (1000 * 60 * 60 * 24));
+  //   var hrs = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //   var mins = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
+  //   var secs = Math.floor((dif % (1000 * 60)) / 1000);
 
-    document.getElementById("counter").innerHTML =
-      days + "d " + hrs + "h " + mins + "m " + secs + "s";
+  //   document.getElementById("counter").innerHTML =
+  //     days + "d " + hrs + "h " + mins + "m " + secs + "s";
 
-    if (dif < 0) {
-      clearInterval(launch);
-    }
-  }, 1000);
+  //   if (dif < 0) {
+  //     clearInterval(launch);
+  //   }
+  // }, 1000);
 });
